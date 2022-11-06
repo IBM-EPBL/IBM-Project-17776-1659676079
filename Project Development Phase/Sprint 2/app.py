d5134090ef42c2df877facd0f3c82f052d6f8838
SARANYA233 <saranyasrissss1523@gmail.com>
2022-11-06 12:12:56 +0530

    sprint updated

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -4,36 +4,10 @@ import pickle
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
-class my_dictionary(dict):
-  def __init__(self):
-    self = dict()
-  def add(self, key, value):
-    self[key] = value
-database=my_dictionary()
 
-@app.route('/form_reg',methods=['POST','GET'])
-def reg():
-    name2=request.form['userid']
-    pwd1=request.form['pwd']
-    database.add(name2,pwd1)
-    return render_template("index.html")
-@app.route('/form_login',methods=['POST','GET'])
-def login():
-    name1=request.form['userid']
-    pwd=request.form['pwd']
-    if name1 not in database:
-	    return render_template('index.html',info='Invalid User!!')
-    else:
-        if database[name1]!=pwd:
-            return render_template('index.html',info='Invalid Password!!')
-        else:
-	         return render_template('home.html',name=name1)
 @app.route("/") 
 def about():
-    return render_template("home.html")#rendering html page
+    return render_template("home.html")
 
 @app.route("/home") 
 def home():
